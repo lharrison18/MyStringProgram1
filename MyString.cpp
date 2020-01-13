31a55dcf646f51f68955675fb6a6e72daa09342e
Lauren Harrison <laurenharrison0322@gmail.com>
2020-01-13 13:05:26 -0700

    adding header file

--- MyString.cpp
+++ MyString.cpp
@@ -3,12 +3,17 @@
 //Date: 1/10/20 
 //Discription Writing a program that will let you change an array behind the scenes
 
+#include <iostream>
+#include <iomanip>
+#include <string.h>
+
 using namespace std;
 
 class MyString
 {
 public:
 	myString str;
+	currentLength;
 
 
 
@@ -16,16 +21,23 @@ public:
 MyString::myString str
 {
 	arr == nullptr;
-    curr.length = 0;
+	curr.length = 0;
 	capacity = 0;
+
+
+}
+ MyString::currentLength
+{
+
 
 
 }
 int main() {
 
-
-
-
+	int curr.length;
+	int capacity;
+	char* arr;
+	   
 
 	return 0;
 }
---
+++ string.h
@@ -0,0 +1,24 @@
+#pragma once
+class MyString
+{
+public:
+	myString str;
+	currentLength;
+
+
+
+};
+MyString::myString str
+{
+	arr == nullptr;
+	curr.length = 0;
+	capacity = 0;
+
+
+}
+MyString::currentLength
+{
+
+
+
+}
