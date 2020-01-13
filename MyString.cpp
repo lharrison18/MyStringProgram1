04884f549340568627d8e863e538b174a0d7515a
Lauren Harrison <laurenharrison0322@gmail.com>
2020-01-13 12:32:22 -0700

    add more to the class

--- MyString.cpp
+++ MyString.cpp
@@ -1,12 +1,31 @@
 //Title: MyStringProgram1
 //Name: Lauren Harrison
-//Date: 1/ /20 
+//Date: 1/10/20 
 //Discription Writing a program that will let you change an array behind the scenes
 
 using namespace std;
 
-Class MyString
+class MyString
 {
+public:
+	myString str;
+
 
 
 };
+MyString::myString str
+{
+	arr == nullptr;
+    curr.length = 0;
+	capacity = 0;
+
+
+}
+int main() {
+
+
+
+
+
+	return 0;
+}
